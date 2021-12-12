ccb2f266f3c6e567b94891adadac1c137991358a
CasiaFan <fanzongshaoxing@gmail.com>
2021-12-12 13:49:57 -0600

    server fix

--- run_eval.sh
+++ run_eval.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
-model_dir=""
+model_dir="test/resnet18_None_loss_CE_dataset_BIRD_partial_False"
 declare -a StringArray=("best_model.pt")
 for model in ${StringArray[@]};
 do
     full_path="$model_dir/$model"
     python eval.py --feat_name="resnet18" \
-               --mask_name="mask_rasaee"
+               --mask_name=None \
                --num_classes=2 \
                --model_weights=$full_path \
                --image_size=224 \
                --device="cuda:0" \
-               --dataset="BUSI" \
+               --dataset="BIRD" \
                --multi_gpu=False \
                --acc
             #    image2mask \
